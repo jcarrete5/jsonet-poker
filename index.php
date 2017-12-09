54720f43f00b1f444f330bb9e8c69dc703ab3050
Jason Carrete <jasoncarrete5@gmail.com>
2017-12-09 00:42:43 -0500

    Added jquery api

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8">
 		<meta name="author" content="Jason R. Carrete">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="icon" href="favicon.png">
 		<title>Jsonet - Poker</title>
