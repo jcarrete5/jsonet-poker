96c6375607bfcf0ad20afda8147333a81576e5be
Jason Carrete <jasoncarrete5@gmail.com>
2017-12-09 23:25:58 -0500

    Styled header and added google sign in button (no-impl)

--- index.php
+++ index.php
@@ -4,14 +4,19 @@
 		<meta charset="UTF-8">
 		<meta name="author" content="Jason R. Carrete">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<meta name="google-signin-client_id" content="421124575022-tquk5nf0l5eteslsf4kro38fk212crj8.apps.googleusercontent.com">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" defer></script>
+		<script src="https://apis.google.com/js/platform.js" async defer></script>
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="icon" href="favicon.png">
 		<title>Jsonet - Poker</title>
 	</head>
 	<body>
-		<header>
-			<h1>Jsonet Poker</h1>
+		<header id="web-page-header">
+			<h1 class="title">
+				Js<img class="lowercase" src="img/poker_chip.png" alt="o">net P<img class="lowercase" src="img/poker_chip.png" alt="o">ker
+			</h1>
+			<div class="g-signin2" data-onsuccess="onSignIn"></div>
 		</header>
 	</body>
 </html>
